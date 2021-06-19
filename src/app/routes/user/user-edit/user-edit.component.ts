6347a3a2795abc84e65211095da6cfb3c4cdd492
Dang Nguyen <pandabamboo90@gmail.com>
2021-06-20 02:19:10 +0700

    feat: update user edit, allow user to select roles and correctly send data to server follow JSON API format

--- src/app/routes/user/user-edit/user-edit.component.ts
+++ src/app/routes/user/user-edit/user-edit.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Role, RoleApi, User, UserApi } from '@core';
 import { SFCheckboxWidgetSchema, SFComponent, SFSchema } from '@delon/form';
-import { SFSchemaEnumType } from '@delon/form/src/schema';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { ErrorResponse, JsonApiError } from '@shared';
-import { each as _each, map as _map, omit as _omit, pick as _pick } from 'lodash-es';
+import { each as _each, map as _map, omit as _omit } from 'lodash-es';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { DocumentCollection } from 'ngx-jsonapi';
 import { Observable } from 'rxjs';
-import { concatMap, filter, finalize, map } from 'rxjs/operators';
+import { concatMap, filter, finalize, tap } from 'rxjs/operators';
 
 @UntilDestroy()
 @Component({
@@ -24,8 +23,9 @@ export class UserUserEditComponent implements OnInit {
   title!: string;
   subTitle!: string;
   user!: User;
+  roles!: Role[];
   loading = false;
-  formData!: any;
+  requiredAttrs: string[] = ['name', 'email', 'mobile_phone', 'password', 'confirm_password'];
   schema: SFSchema = {
     type: 'object',
     properties: {
@@ -45,7 +45,7 @@ export class UserUserEditComponent implements OnInit {
             type: 'string',
             title: 'Mobile',
             minLength: 9,
-            maxLength: 12,
+            maxLength: 20,
             pattern: '[0-9]+',
           },
           password: {
@@ -65,18 +65,22 @@ export class UserUserEditComponent implements OnInit {
             },
           },
         },
-        required: ['name', 'email', 'mobile_phone', 'password', 'confirm_password'],
+        required: this.requiredAttrs,
       },
       relationships: {
         type: 'object',
         properties: {
           roles: {
-            type: 'string',
-            title: 'Roles',
-            ui: {
-              widget: 'checkbox',
-              // asyncData: this.fetchRoleList.bind(this),
-            } as SFCheckboxWidgetSchema,
+            type: 'object',
+            properties: {
+              data: {
+                type: 'string',
+                title: 'Roles',
+                ui: {
+                  widget: 'checkbox',
+                } as SFCheckboxWidgetSchema,
+              },
+            },
           },
         },
       },
@@ -94,37 +98,42 @@ export class UserUserEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params.id.toString();
-    // this.fetchRoleList();
 
     if (this.id === 'new') {
       this.title = 'Add User';
       this.subTitle = ``;
       this.user = this.userApi.new();
+
+      this.fetchRoleList()
+        .subscribe(() => {
+          this.buildRoleCheckboxes([]);
+        });
+
     } else {
       this.title = `Edit User`;
       this.subTitle = `ID: ${this.id}`;
 
       this.fetchRoleList()
         .pipe(
-          map((roleEnum) => this.schema.properties!.relationships.properties!.roles.enum = roleEnum),
           concatMap(() => this.fetchUserById(this.id)),
-          map((user: User) => {
-            this.user = user;
-            this.formData = _pick(user, 'attributes');
-            this.updateUIForEditForm();
-          }),
         )
-        .subscribe();
+        .subscribe(() => {
+          this.buildRoleCheckboxes(_map(this.user.relationships.roles.data, 'id'));
+          this.setAttrValues();
+          this.updateUIForEditForm();
+        });
     }
   }
 
   submit(formData: any): void {
-    this.loading = true;
-    this.user = formData;
+    const selectedRoleIds = this.sf.getProperty('/relationships/roles/data')?.value;
+    const selectedRoles = this.roles.filter((role) => selectedRoleIds.indexOf(role.id) > -1);
+    this.user.addRelationships(selectedRoles, 'roles');
 
     this.user
       .save()
       .pipe(
+        tap(() => this.loading = true),
         finalize(() => this.loading = false), // Success or not, turn off loading
       )
       .subscribe(res => {
@@ -141,37 +150,49 @@ export class UserUserEditComponent implements OnInit {
   }
 
   fetchUserById(id: string): Observable<User> {
-    console.log('fetchUserById');
     return this.userApi.get(id)
       .pipe(
         untilDestroyed(this),
         filter(res => res.loaded),
+        tap((res) => this.user = res),
       );
   }
 
-  fetchRoleList(): Observable<SFSchemaEnumType[]> {
+  fetchRoleList(): Observable<DocumentCollection<Role>> {
     return this.roleApi.all()
       .pipe(
         untilDestroyed(this),
         filter(res => res.loaded),
-        map((res: DocumentCollection<Role>) => {
-          const roleEnum: SFSchemaEnumType[] = _map(res.data, (item: Role) => {
-            return { label: item.attributes.display_name, value: item.id };
-          });
-
-          return roleEnum;
-        }),
+        tap((res) => this.roles = res.data),
       );
   }
 
   private updateUIForEditForm(): void {
     // Set email as readonly field
-    this.schema.properties!.attributes.properties!.email.readOnly = true;
-    // Remove the password fields as we dont' want user to accidentally update password
-    this.schema.properties!.attributes.properties = _omit(this.schema.properties!.attributes.properties, ['password', 'confirm_password']) as any;
-    this.schema.properties!.attributes.required = _omit(this.schema.properties!.attributes.required, ['password', 'confirm_password']) as string[];
+    this.sf.getProperty('/attributes/email')!.schema.readOnly = true;
 
-    // Set default value
-    this.schema.properties!.relationships.properties!.roles.default = _map(this.user.relationships.roles.data, 'id')
+    // Remove the password fields as we dont' want user to accidentally update password
+    this.sf.getProperty('/attributes')!.schema.required = _omit(this.requiredAttrs, ['password', 'confirm_password']) as string[];
+    this.sf.getProperty('/attributes/password')?.setVisible(false);
+    this.sf.getProperty('/attributes/confirm_password')?.setVisible(false);
+  }
+
+  private buildRoleCheckboxes(initialValues: string[] = []) {
+    const rolesProp = this.sf.getProperty('/relationships/roles/data');
+
+    rolesProp!.schema.enum = _map(this.roles, (item: Role) => {
+      return { label: item.attributes.display_name, value: item.id };
+    });
+    rolesProp!.resetValue(initialValues, true);
+  }
+
+  private setAttrValues() {
+    _each(this.user.attributes, (attrValue, attrKey) => {
+      const formProp = this.sf.getProperty(`/attributes/${attrKey}`);
+
+      if (formProp) {
+        this.sf.setValue(`/attributes/${attrKey}`, attrValue);
+      }
+    });
   }
 }
